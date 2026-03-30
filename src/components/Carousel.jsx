d356c4d008b167c89e0a2eb75e04c380194b3698
vishnu <kolluruvishnuvardhan990@gmail.com>
2026-03-30 08:34:52 +0530

    utilized updater function to set state values

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () =>
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+
+  const handlePrevious = () =>
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
+
+  return (
+    <div className="flex items-center">
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Left}
+        style="text"
+        onClick={handlePrevious}
+      />
+      <img
+        alt={title}
+        className="max-w-56 h-56 max-h-56 w-56"
+        src={imageUrls[currentIndex]}
+      />
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Right}
+        style="text"
+        onClick={handleNext}
+      />
+    </div>
+  );
+};
+
+export default Carousel;
